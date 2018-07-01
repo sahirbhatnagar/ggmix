f0f27f35f89a9b3b29acd3e9017b764f24f8ad12
Sahir Bhatnagar <sahir.bhatnagar@gmail.com>
2018-07-01 00:18:10 -0400

    added simulation results to manuscript

--- manuscript/bin/setup.R
+++ manuscript/bin/setup.R
@@ -5,11 +5,11 @@ if (!requireNamespace("pacman", quietly = TRUE)) {
 }
 
 # pacman::p_load_gh("sahirbhatnagar/sail", dependencies = FALSE)
-devtools::load_all("/home/sahir/git_repositories/sail/")
+devtools::load_all("/home/sahir/git_repositories/ggmix/")
 # devtools::load_all("C:/Users/sahir/Documents/git_repositories/sail")
 pacman::p_load(ggplot2)
 pacman::p_load(doMC)
-registerDoMC(cores = 8)
+doMC::registerDoMC(cores = 8)
 pacman::p_load(latex2exp)
 pacman::p_load(truncnorm)
 pacman::p_load(lemon)
@@ -19,6 +19,7 @@ pacman::p_load(dplyr)
 pacman::p_load(tidyr)
 pacman::p_load(data.table)
 pacman::p_load(psych)
+pacman::p_load(RSkittleBrewer)
 # pacman::p_load(cowplot)
 pacman::p_load_gh("hrbrmstr/hrbrthemes")
 pacman::p_load(ggrepel)
@@ -31,5 +32,6 @@ extrafont::loadfonts()
 
 cbbPalette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
 trop <- RSkittleBrewer::RSkittleBrewer("trop")
-gg_sy <- theme(legend.position = "bottom", axis.text = element_text(size = 20),
-               axis.title = element_text(size = 20), legend.text = element_text(size = 20), legend.title = element_text(size = 20))
+gg_sy <- ggplot2::theme(legend.position = "bottom", axis.text = element_text(size = 20),
+                        axis.title = element_text(size = 20), legend.text = element_text(size = 20),
+                        legend.title = element_text(size = 20))
--- manuscript/bin/simulation.R
+++ manuscript/bin/simulation.R
@@ -1,152 +1,153 @@
 ## ---- simulation-results ----
 
-df <- readRDS("/home/sahir/git_repositories/sail/my_sims/simulation_results/may_15_2018_results.rds")
-# df <- readRDS("C:/Users/sahir/Documents/git_repositories/sail/my_sims/simulation_results/apr_25_2018_results.rds")
-df <- df %>% separate(Model, into = c("simnames","betaE","corr","lambda.type","n","p","parameterIndex","SNR_2"),
+df <- readRDS("/home/sahir/git_repositories/ggmix/simulation/simulation_results/june_29_2018_results.rds")
+df <- df %>% separate(Model,
+                      into = c("simnames","b0","eta","Fst","geography","k","n",
+                               "pkinship","ptest","percentcausal",
+                               "percentoverlap","s","sigma2"),
                       sep = "/")
+DT <- data.table::as.data.table(df, stringsAsFactors = FALSE)
 
-DT <- as.data.table(df, stringsAsFactors = FALSE)
-DT <- DT[parameterIndex != "parameterIndex_4"]
-# DT[parameterIndex=="parameterIndex_1", table(Method)] %>% names %>% dput
+# DT[, table(percentoverlap, p_overlap)]
+DT[, p_overlap := case_when(percentoverlap == "percent_overlap_0" ~ "No causal SNPs in Kinship",
+                            percentoverlap == "percent_overlap_100" ~ "All causal SNPs in Kinship")]
+
+DT[geography == "geography_ind", structure := "block"]
+DT[geography == "geography_circ", structure := "circular"]
+DT[geography == "geography_1d", structure := "1D"]
+# DT[, table(geography, structure)]
+DT[, structure := factor(structure, levels = c("block","1D","circular"))]
+DT[, Method := factor(Method, levels = c("twostep","lasso","ggmix"))]
 # DT[, table(Method)]
-
-DT[Method=="Adaptivesail", Method := "adaptive sail"]
-# DT[Method=="Adaptivesailweak", Method := "Asail weak"]
-DT[Method=="Adaptivelasso", Method := "adaptive lasso"]
-DT[Method=="sailweak", Method := "sail weak"]
-# DT[Method=="sail", Method := "sail strong"]
-# DT[Method=="Asail", Method := "Asail strong"]
-DT[Method=="linearsail", Method := "linear sail"]
-DT[, Method := droplevels(Method)]
-# DT[, table(Method)]
-# DT[, table(Method)] %>% names %>% dput
-appender <- function(string) TeX(paste(string))
-
-# DT[, method := factor(Method, levels = c("lasso","Alasso","lassoBT", "GLinternet", "HierBasis", "SPAM", "gamsel",
-#                                        "sail strong", "Asail strong", "sail weak", "Asail weak"))]
-
-DT[, method := factor(Method, levels = c("lasso","adaptive lasso","lassoBT", "GLinternet", "HierBasis", "SPAM", "gamsel",
-                                         "sail", "adaptive sail",  "sail weak", "linear sail"))]
-
-# DT[, table(method)]
-# DT[, table(parameterIndex)]
-DT[, scenario:= as.numeric(as.character(stringr::str_extract_all(parameterIndex, "\\d", simplify = T)))]
-# DT[, table(scenario)]
-DT[, scenario := replace(scenario, which(scenario==6), 4)]
-DT[, scen := case_when(scenario==1 ~ "1a) Strong Hierarchy",
-                       scenario==2 ~ "1b) Weak Hierarchy",
-                       scenario==3 ~ "1c) Interactions Only",
-                       scenario==4 ~ "2) Linear Effects",
-                       scenario==5 ~ "3) Main Effects Only")]
-DT[, scen := factor(scen, levels = c("1a) Strong Hierarchy", "1b) Weak Hierarchy","1c) Interactions Only","2) Linear Effects", "3) Main Effects Only"))]
-# DT$scen %>% table
-#Truth obeys strong hierarchy (parameterIndex = 1)
-#Truth obeys weak hierarchy (parameterIndex = 2)
-#Truth only has interactions (parameterIndex = 3)
-#Truth is linear (parameterIndex = 4)
-#Truth only has main effects (parameterIndex = 5)
+appender <- function(string) latex2exp::TeX(paste(string))
 
 
-## ---- plot-mse-sim ----
+## ---- plot-correct-sparsity-sim ----
 
-p1_mse <- ggplot(DT, aes(method, mse, fill = method)) +
+p1_cs <- ggplot(DT, aes(Method, correct_sparsity, fill = Method)) +
     ggplot2::geom_boxplot() +
     # gg_sy +
     # facet_rep_wrap(~scen, scales = "free", ncol = 2,
     #                repeat.tick.labels = 'left',
     #                labeller = as_labeller(appender,
     #                                       default = label_parsed)) +
-    facet_rep_wrap(~scen, scales = "free", ncol = 2,
+    facet_rep_grid(p_overlap ~ structure, scales = "fixed",
                    repeat.tick.labels = 'left',
                    labeller = as_labeller(appender,
                                           default = label_parsed)) +
-    scale_fill_manual(values=RColorBrewer::brewer.pal(12, "Paired")[-11], guide=guide_legend(ncol=3)) +
-    # ggplot2::labs(y = "Test Set MSE", title = "") + xlab("") +
-    labs(x="", y="Test Set MSE",
-         title="Test Set MSE",
-         subtitle="Based on 200 simulations",
-         caption="") +
+    scale_fill_manual(values = cbbPalette[c(7,3,4)]) +
+    labs(x = "", y = "",
+         title = "Correct Sparsity",
+         subtitle = "Based on 200 simulations",
+         caption = "") +
     # panel_border()+
     # background_grid()+
-    theme_ipsum_rc() + theme(legend.position = "right", axis.text.x = element_text(angle = 25, hjust = 1),
-                             legend.text=element_text(size=14),
-                             strip.text = element_text(size=14))
+    theme_ipsum_rc() +
+  theme(legend.position = "bottom",title = element_text(size = 20),
+        axis.text.x = element_text(angle = 0, hjust = 1, size = 16),
+        legend.text = element_text(size = 16), legend.title = element_text(size = 16),
+        strip.text = element_text(size = 18))
 
 # , legend.text=element_text(size=18)
 
+p1_cs
+# reposition_legend(p1_mse, 'center', panel='panel-2-3')
 
-reposition_legend(p1_mse, 'center', panel='panel-2-3')
+## ---- plot-me-nactive-sim ----
 
-## ---- plot-mse-nactive-sim ----
+df_me_nactive <- DT[, c("Method", "structure", "p_overlap", "nactive", "me")] %>%
+  group_by(Method, structure, p_overlap) %>%
+  summarise(mean.me = mean(me, na.rm = TRUE), sd.me = sd(me, na.rm = TRUE),
+         mean.nactive = mean(nactive, na.rm = TRUE), sd.nactive = sd(nactive, na.rm = TRUE))
 
-
-df_mse_nactive <- DT[, c("method","scen","mse","nactive")] %>%
-  group_by(method, scen) %>%
-  summarise(mean.mse = mean(mse, na.rm = TRUE), sd.mse = sd(mse, na.rm = TRUE),
-         mean.nactive = mean(nactive, na.rm = TRUE), sd.nactive = sd(nactive, na.rm = TRUE)) %>%
-  mutate(scen = case_when(scen == "1a) Strong Hierarchy" ~ "1a) Strong Hierarchy (|S_0| = 7)",
-                          scen == "1b) Weak Hierarchy" ~ "1b) Weak Hierarchy (|S_0| = 5)",
-                          scen == "1c) Interactions Only" ~ "1c) Interactions Only (|S_0| = 2)",
-                          scen == "2) Linear Effects" ~ "2) Linear Effects (|S_0| = 7)",
-                          scen == "3) Main Effects Only" ~ "3) Main Effects Only (|S_0| = 5)")) %>%
-  mutate(scen = factor(scen, levels = c("1a) Strong Hierarchy (|S_0| = 7)",
-                                        "1b) Weak Hierarchy (|S_0| = 5)",
-                                        "1c) Interactions Only (|S_0| = 2)",
-                                        "2) Linear Effects (|S_0| = 7)",
-                                        "3) Main Effects Only (|S_0| = 5)")))
-
-p1_mse_nactive <- ggplot(data = df_mse_nactive, aes(x = mean.nactive, y = mean.mse, color = method, label = method)) +
+p1_me_nactive <- ggplot(data = df_me_nactive, aes(x = mean.nactive, y = mean.me, color = Method, label = Method)) +
   geom_point(size = 2.1) +
   geom_text_repel(
-    data = subset(df_mse_nactive, mean.nactive < 100),
-    nudge_x      = 40,
+    data = subset(df_me_nactive, mean.nactive < 100),
+    nudge_x      = 5,
+    nudge_y = 9,
+    # size = 8,
     direction    = "y",
     hjust        = 0,
     segment.size = 0.2
   ) +
   geom_text_repel(
-    data = subset(df_mse_nactive, mean.nactive >= 100),
+    data = subset(df_me_nactive, mean.nactive >= 100),
     nudge_x      = 5,
+    nudge_y = 9,
+    # size = 8,
     direction    = "y",
     hjust        = 0,
     segment.size = 0.2
   ) +
-  geom_errorbar(aes(ymin = mean.mse - sd.mse, ymax = mean.mse + sd.mse), size = 1.1) +
+  geom_errorbar(aes(ymin = mean.me - sd.me, ymax = mean.me + sd.me), size = 1.1) +
   geom_errorbarh(aes(xmin = mean.nactive - sd.nactive, xmax = mean.nactive + sd.nactive), size = 1.1) +
-  facet_rep_wrap(~scen, scales = "free", ncol = 2,
+  facet_rep_grid(p_overlap ~ structure, scales = "free",
                  repeat.tick.labels = 'left',
                  labeller = as_labeller(appender,
                                         default = label_parsed)) +
   # scale_color_brewer(palette = "Dark2")+
-  scale_color_manual(values=RColorBrewer::brewer.pal(12, "Paired")[-11], guide=guide_legend(ncol=3)) +
-  labs(x="Number of active variables", y="Test Set MSE",
-       title="Test Set MSE vs. Number of Active Variable (Mean +/- 1 SD)",
-       subtitle="Based on 200 simulations",
-       caption="") +
-  theme_ipsum_rc(axis_title_just = "bt") +
-  theme(legend.position = "right",
-        legend.text=element_text(size=14),
-        strip.text = element_text(size=14))
+  scale_color_manual(values = cbbPalette[c(7,3,4)], guide = guide_legend(ncol=3)) +
+  labs(x = "Number of active variables", y = "Model Error",
+       title = "Model Error vs. Number of Active Variable (Mean +/- 1 SD)",
+       subtitle = "Based on 200 simulations",
+       caption = "") +
+  theme_ipsum_rc(axis_title_just = "bt",axis_title_size = 16) +
+    theme(legend.position = "bottom",title = element_text(size = 20),
+          axis.text.x = element_text(angle = 0, hjust = 1, size = 16),
+          legend.text = element_text(size = 16), legend.title = element_text(size = 16),
+          strip.text = element_text(size = 18))
 
-reposition_legend(p1_mse_nactive, 'center', panel='panel-2-3')
+p1_me_nactive
 
-# pacman::p_load(psych)
-# affect.mat2 <- describeBy(DT[, c("mse","nactive")], group = list(DT$method, DT$scen), mat = TRUE)
-# dev.off()
-# par(family="serif")
-# error.crosses(affect.mat2[c(56:66),],
-#               affect.mat2[c(1:11),],
-#               labels=unique(affect.mat2$group1),
-#               xlab="Number of Active Variables",
-#               main = "ADNI Data: Means (+/- 1 SD) from 200 Train/Validate/Test Splits",
-#               sd = TRUE,
-#               cex.lab = 1.4,
-#               cex.axis = 1.4,
-#               cex.main = 1.5,
-#               # xlim = c(0, 34),
-#               ylab="Test Set MSE",
-#               colors = RColorBrewer::brewer.pal(11, "Paired"),
-#               pch=16,cex=2)
+
+## ---- plot-prederror-nactive-sim ----
+
+df_prederror_nactive <- DT[, c("Method", "structure", "p_overlap", "nactive", "prederror")] %>%
+  group_by(Method, structure, p_overlap) %>%
+  summarise(mean.prederror = mean(prederror, na.rm = TRUE), sd.prederror = sd(prederror, na.rm = TRUE),
+            mean.nactive = mean(nactive, na.rm = TRUE), sd.nactive = sd(nactive, na.rm = TRUE))
+
+p1_prederror_nactive <- ggplot(data = df_prederror_nactive, aes(x = mean.nactive, y = mean.prederror, color = Method, label = Method)) +
+  geom_point(size = 2.1) +
+  geom_text_repel(
+    data = subset(df_prederror_nactive, mean.nactive < 100),
+    nudge_x      = 5,
+    nudge_y = 9,
+    # size = 8,
+    direction    = "y",
+    hjust        = 0,
+    segment.size = 0.2
+  ) +
+  geom_text_repel(
+    data = subset(df_prederror_nactive, mean.nactive >= 100),
+    nudge_x      = 5,
+    nudge_y = 9,
+    # size = 8,
+    direction    = "y",
+    hjust        = 0,
+    segment.size = 0.2
+  ) +
+  geom_errorbar(aes(ymin = mean.prederror - sd.prederror, ymax = mean.prederror + sd.prederror), size = 1.1) +
+  geom_errorbarh(aes(xmin = mean.nactive - sd.nactive, xmax = mean.nactive + sd.nactive), size = 1.1) +
+  facet_rep_grid(p_overlap ~ structure, scales = "free",
+                 repeat.tick.labels = 'left',
+                 labeller = as_labeller(appender,
+                                        default = label_parsed)) +
+  # scale_color_brewer(palette = "Dark2")+
+  scale_color_manual(values = cbbPalette[c(7,3,4)], guide = guide_legend(ncol=3)) +
+  labs(x = "Number of active variables", y = "Prediction Error",
+       title = "Model Error vs. Number of Active Variable (Mean +/- 1 SD)",
+       subtitle = "Based on 200 simulations",
+       caption = "") +
+  theme_ipsum_rc(axis_title_just = "bt",axis_title_size = 16) +
+  theme(legend.position = "bottom",title = element_text(size = 20),
+        axis.text.x = element_text(angle = 0, hjust = 1, size = 16),
+        legend.text = element_text(size = 16), legend.title = element_text(size = 16),
+        strip.text = element_text(size = 18))
+
+p1_prederror_nactive
+
 
 ## ---- plot-tpr-sim ----
 
@@ -192,79 +193,73 @@ reposition_legend(p1_fpr, 'center', panel='panel-2-3')
 
 ## ---- plot-nactive-sim ----
 
-p1_nactive <- ggplot(DT, aes(method, nactive, fill = method)) +
+p1_nactive <- ggplot(DT, aes(Method, nactive, fill = Method)) +
   ggplot2::geom_boxplot() +
-  facet_rep_wrap(~scen, scales = "free", ncol = 2,
+  facet_rep_grid(p_overlap ~ structure, scales = "fixed",
                  repeat.tick.labels = 'left',
                  labeller = as_labeller(appender,
                                         default = label_parsed)) +
-  scale_fill_manual(values=RColorBrewer::brewer.pal(12, "Paired")[-11], guide=guide_legend(ncol=3)) +
-  labs(x="", y="Number of active variables",
-       title="Number of active variables",
-       subtitle="Based on 200 simulations",
-       caption="") +
-  theme_ipsum_rc() + theme(legend.position = "right", axis.text.x = element_text(angle = 25, hjust = 1),
-                           legend.text=element_text(size=14),
-                           strip.text = element_text(size=14))
+  scale_fill_manual(values = cbbPalette[c(7,3,4)]) +
+  labs(x = "", y = "",
+       title = "Number of Active Variables",
+       subtitle = "Based on 200 simulations",
+       caption = "") +
+  theme_ipsum_rc() +
+  theme(legend.position = "bottom",title = element_text(size = 20),
+        axis.text.x = element_text(angle = 0, hjust = 1, size = 16),
+        legend.text = element_text(size = 16), legend.title = element_text(size = 16),
+        strip.text = element_text(size = 18))
 
-
-reposition_legend(p1_nactive, 'center', panel='panel-2-3')
+p1_nactive
 
 
 
 
 ## ---- plot-tpr-fpr-sim ----
 
-DT[fpr==1, fpr:=NA] # this is skewing the plots, so we remove them and set to NA
-df_tpr_fpr <- DT[, c("method","scen","tpr","fpr")] %>%
-  group_by(method, scen) %>%
+df_tpr_fpr <- DT[, c("Method", "structure", "p_overlap", "tpr", "fpr")] %>%
+  group_by(Method, structure, p_overlap) %>%
   summarise(mean.tpr = mean(tpr, na.rm = TRUE), sd.tpr = sd(tpr, na.rm = TRUE),
-            mean.fpr = mean(fpr, na.rm = TRUE), sd.fpr = sd(fpr, na.rm = TRUE)) %>%
-  mutate(scen = case_when(scen == "1a) Strong Hierarchy" ~ "1a) Strong Hierarchy (|S_0| = 7)",
-                          scen == "1b) Weak Hierarchy" ~ "1b) Weak Hierarchy (|S_0| = 5)",
-                          scen == "1c) Interactions Only" ~ "1c) Interactions Only (|S_0| = 2)",
-                          scen == "2) Linear Effects" ~ "2) Linear Effects (|S_0| = 7)",
-                          scen == "3) Main Effects Only" ~ "3) Main Effects Only (|S_0| = 5)")) %>%
-  mutate(scen = factor(scen, levels = c("1a) Strong Hierarchy (|S_0| = 7)",
-                                        "1b) Weak Hierarchy (|S_0| = 5)",
-                                        "1c) Interactions Only (|S_0| = 2)",
-                                        "2) Linear Effects (|S_0| = 7)",
-                                        "3) Main Effects Only (|S_0| = 5)")))
+            mean.fpr = mean(fpr, na.rm = TRUE), sd.fpr = sd(fpr, na.rm = TRUE))
 
-p1_tpr_fpr <- ggplot(data = df_tpr_fpr, aes(x = mean.fpr, y = mean.tpr, color = method, label = method)) +
+p1_tpr_fpr <- ggplot(data = df_tpr_fpr, aes(x = mean.fpr, y = mean.tpr, color = Method, label = Method)) +
   geom_point(size = 2.1) +
   # geom_text_repel() +
   geom_text_repel(
-    data = subset(df_tpr_fpr, mean.fpr < 0.05),
-    nudge_x      = 0.02,
+    data = subset(df_tpr_fpr, mean.fpr < 0.02),
+    nudge_x      = 0.002,
     direction    = "y",
     hjust        = 0,
     segment.size = 0.2
   ) +
   geom_text_repel(
-    data = subset(df_tpr_fpr, mean.fpr > 0.05),
-    nudge_x      = 0.00,
+    data = subset(df_tpr_fpr, mean.fpr > 0.02),
+    nudge_x      = 0.005,
+    nudge_y = 0.015,
     direction    = "y",
     hjust        = 0,
     segment.size = 0.2
   ) +
   geom_errorbar(aes(ymin = mean.tpr - sd.tpr, ymax = mean.tpr + sd.tpr), size = 1.1) +
   geom_errorbarh(aes(xmin = mean.fpr - sd.fpr, xmax = mean.fpr + sd.fpr), size = 1.1) +
-  facet_rep_wrap(~scen, scales = "free", ncol = 2,
+  facet_rep_grid(p_overlap ~ structure, scales = "free",
                  repeat.tick.labels = 'left',
                  labeller = as_labeller(appender,
                                         default = label_parsed)) +
-  scale_color_manual(values=RColorBrewer::brewer.pal(12, "Paired")[-11], guide=guide_legend(ncol=3)) +
+  scale_color_manual(values = cbbPalette[c(7,3,4)], guide=guide_legend(ncol=3)) +
   labs(x="False positive rate", y="True positive rate",
        title="True Positive Rate vs. False Positive Rate (Mean +/- 1 SD)",
        subtitle="Based on 200 simulations",
        caption="") +
-  theme_ipsum_rc(axis_title_just = "bt") +
-  theme(legend.position = "right",
-        legend.text=element_text(size=14),
-        strip.text = element_text(size=14))
+  theme_ipsum_rc(axis_title_just = "bt",axis_title_size = 16) +
+  theme(legend.position = "bottom",title = element_text(size = 20),
+        axis.text.x = element_text(angle = 0, hjust = 1, size = 16),
+        legend.text = element_text(size = 16), legend.title = element_text(size = 16),
+        strip.text = element_text(size = 18))
 
-reposition_legend(p1_tpr_fpr, 'center', panel='panel-2-3')
+p1_tpr_fpr
+
+# reposition_legend(p1_tpr_fpr, 'center', panel='panel-2-3')
 
 
 ## ---- plot-fpr-tpr-boxplot-sim ----
