06332eb7c7f86d224901d4e8705705341cddfc10
sahir <sahir.bhatnagar@gmail.com>
2019-04-26 16:42:19 -0400

    sim_trait simulates the data with a certain heritability , but i cant separate
    whats in the kinship and whats not in terms of causaul betas. so I will try to adopt the existing framework, to the prediction problem. see git_repositories/ggmix/rda/mice/rmse_individual_for_3_methods.R

--- simulation/sim_trait.R
+++ simulation/sim_trait.R
@@ -2,7 +2,7 @@ library(bnpsd) # to simulate an admixed population
 
 # dimensions of data/model
 m <- 10000 # number of loci
-n_ind <- 500 # number of individuals, smaller than usual for easier visualizations
+n_ind <- 2000 # number of individuals, smaller than usual for easier visualizations
 k <- 3 # number of intermediate subpops
 
 # define population structure
@@ -34,7 +34,7 @@ p_anc <- out$Pa # ancestral AFs
 library(simtrait) # load this package
 
 # parameters of simulation
-m_causal <- 5
+m_causal <- m * 0.005
 herit <- 0.4
 
 # create simulated trait and associated data
@@ -49,6 +49,8 @@ obj <- sim_trait(X=X, m_causal=m_causal, herit=herit, p_anc=p_anc)
 # outputs in both versions:
 # trait vector
 obj$y
+library(magrittr)
+obj$y %>% hist
 # randomly-picked causal locus index
 obj$i
 # locus effect size vector
@@ -68,7 +70,8 @@ Xtest <- t(X[,-ind])
 dim(Xtrain)
 dim(Xtest)
 
-plotPopkin(Phi)
+library(popkin)
+popkin::plotPopkin(Phi)
 
 Phi_train <- Phi[ind,ind]
 dim(Phi_train)
@@ -91,17 +94,45 @@ hdbic <- gic(fit, an = log(length(obj$y)))
 plot(hdbic)
 # hdbic <- gic(fit)
 # plot(hdbic)
-dev.off()
-nzcoef <- rownames(coef(hdbic, type = "nonzero"))
-nzcoef
-paste0("V",obj$i) %in% nzcoef
+# dev.off()
+# help(ggmix:::coef.ggmix_gic)
+nzcoef_ggmix <- setdiff(rownames(coef(hdbic, type = "nonzero")), c("(Intercept)","eta","sigma2"))
+nzcoef_ggmix
+
+# true positive rate
+sum(nzcoef_ggmix %in% paste0("V",obj$i)) / length(obj$i)
+
+# false positive rate
+FP <- length(setdiff(nzcoef_ggmix, paste0("V",obj$i))) # false positives
+TN <- m-m_causal # True negatives
+FPR <- FP / (FP + TN)
+FPR
+
+correct_sparsity <- function(nzcoef, obj, p = m){
+                                 causal <- paste0("V",obj$i)
+                                 not_causal <- setdiff(paste0("V",1:p),paste0("V",obj$i))
+                                 active <- nzcoef
+
+
+                                 correct_nonzeros <- sum(active %in% causal)
+                                 correct_zeros <- length(setdiff(not_causal, active))
+                                 #correct sparsity
+                                 (1 / p) * (correct_nonzeros + correct_zeros)
+                               }
+
+correct_sparsity(nzcoef = nzcoef_ggmix, obj = obj, p = m)
 
 yhat <- predict(hdbic, s="lambda.min", newx = Xtest, type = "individual", covariance = 2*Phi_test_train)
 l2norm(yhat-ytest)
 cor(yhat,ytest)
+dev.off()
+plot(yhat, ytest)
+abline(a=0,b=1)
 
 PC <- prcomp(Xtrain)
 xtrain_lasso <- cbind(Xtrain, PC$x[,1:10])
+
+library(glmnet)
 
 fit_glmnet <- cv.glmnet(x = xtrain_lasso,
                         y = ytrain,
@@ -109,9 +140,12 @@ fit_glmnet <- cv.glmnet(x = xtrain_lasso,
                         standardize = T,
                         penalty.factor = c(rep(1, ncol(Xtrain)), rep(0,10)))
 plot(fit_glmnet)
-nzcoef <- rownames(coef(fit_glmnet, s = "lambda.min")[nonzeroCoef(coef(fit_glmnet, s = "lambda.min")),,drop=FALSE])
-nzcoef
-paste0("V",obj$i) %in% nzcoef
+nzcoef_lasso <- setdiff(rownames(coef(fit_glmnet, s = "lambda.min")[nonzeroCoef(coef(fit_glmnet, s = "lambda.min")),,drop=FALSE]),c("(Intercept)",paste0("PC",1:10)))
+nzcoef_lasso
+
+correct_sparsity(nzcoef = nzcoef_lasso, obj = obj, p = m)
+
+sum(nzcoef %in% paste0("V",obj$i)) / length(obj$i)
 
 xtest_pc <- predict(PC, newdata = Xtest)
 xtest_lasso <- cbind(Xtest, xtest_pc[,1:10])
