3cf419da77613b8db259f1e85151da26889553fb
sahir <sahir.bhatnagar@gmail.com>
2019-12-10 02:13:55 -0500

    done cleaning text, and including LD plots, BSLMM,
    now need to refer to section in response to rev

--- manuscript/bin/RealData.R
+++ manuscript/bin/RealData.R
@@ -152,7 +152,45 @@ p2 <- ggplot(teststats, aes(x=log2(coef),y=RMSE,color=method)) + ylim(0.9,1) + x
         axis.text = element_text(size = 15),
         axis.title = element_text(size = 20),
         legend.text = element_text(size = 15),
-        axis.text.x = element_text(angle = 45,hjust=1,vjust = 1)) +
+        axis.text.x = element_text(angle = 0,hjust=0.5,vjust = 1)) +
   scale_color_manual(values = cbbPalette[c(7,3,4,2)])
 
 p1+p2+plot_annotation(tag_levels = 'A', tag_prefix = "(", tag_suffix = ")")
+
+
+## ---- UKB-chromosome-distribution ----
+
+ukb_df <- data.frame(Chromosome = factor(1:22),
+                     freq = c(493,989,754,645,522,1515,520,388,338,190,141,658,204,300,543,138,292,231,128,962,3,46),
+                     genotyped = c(65,95,62,78,55,284,48,42,47,20,24,71,24,14,77,27,56,20,20,99,1,4))
+# ukb_df$freq %>% sum
+# ukb_df$genotyped %>% sum
+
+ukb_long <- do.call(rbind, lapply(1:nrow(ukb_df), function(i) {
+  data.frame(Chromosome = rep(ukb_df$Chromosome[i], each = ukb_df$freq[i]),
+             Genotyped = c(rep("Genotyped", ukb_df$genotyped[i]), rep("Imputed", ukb_df$freq[i] - ukb_df$genotyped[i]))
+  )
+}
+))
+
+# ukb_long$Chromosome <- as.numeric(ukb_long$Chromosome)
+# str(ukb_long)
+# levels(ukb_long$Genotyped)
+# ukb_long$Genotyped %>% table
+# ukb_long$Chromosome %>% table
+# table(ukb_long$Chromosome, ukb_long$Genotyped) %>% rowSums()
+
+# str(ukb_long)
+
+ggplot(ukb_long, aes(x=Chromosome, fill = Genotyped)) +
+  geom_bar() +
+  ylab("Number of SNPs used in model") +
+  labs(color="") +
+  theme_bw()  +
+  theme(legend.position = "bottom",
+        legend.title = element_blank(),
+        axis.text = element_text(size = 10),
+        axis.title = element_text(size = 20),
+        legend.text = element_text(size = 15),
+        axis.text.x = element_text(angle = 0,hjust=0.5,vjust=0)) +
+  scale_fill_manual(values = cbbPalette[c(7,3)])
